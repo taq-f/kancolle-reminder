f419eddc4eaf053dc2aa1d141d69f68f363a4eac
taq-f <mysecondproject@gmail.com>
2016-06-13 20:04:27 +0900

    遠征帰投メッセージ

---
+++ scripts/kancolle.js
@@ -0,0 +1,71 @@
+// Description:
+//
+//
+// Commands:
+//   遠征に出発しました。帰投予定時刻 yyyy-MM-dd HH:mm:ss - 帰投予定時刻にメッセージを送信
+//   remind mission end at yyyy-MM-dd HH:mm - 帰投予定時刻にメッセージを送信
+
+'use strict';
+
+module.exports = (robot) => {
+
+	robot.hear(/(^遠征に出発しました。帰投予定時刻 )/i, (res) => {
+
+		const message = res.message.text;
+		let remindAt = message.substring('遠征に出発しました。帰投予定時刻 '.length);
+		remindAt = remindAt.split(' ').map(val => val.trim()).filter(val => val !== '');
+
+		const date = remindAt[0].split('-');
+		const time = remindAt[1].split(':');
+		remindAt = date.concat(time);
+
+		remindAt = new Date(
+			remindAt[0],
+			remindAt[1] - 1,
+			remindAt[2],
+			remindAt[3],
+			remindAt[4],
+			remindAt[5]
+		);
+
+		remind(res, remindAt);
+	});
+
+	robot.hear(/(^remind mission end at )/i, (res) => {
+
+		const message = res.message.text;
+		let remindAt = message.substring('remind mission end at '.length);
+		remindAt = remindAt.split(' ').map(val => val.trim()).filter(val => val !== '');
+		const date = remindAt[0].split('-');
+		const time = remindAt[1].split(':');
+		remindAt = date.concat(time);
+
+		remindAt = new Date(
+			remindAt[0],
+			remindAt[1] - 1,
+			remindAt[2],
+			remindAt[3],
+			remindAt[4]
+		);
+
+		remind(res, remindAt);
+	});
+};
+
+function remind(res, remindAt) {
+
+	const remindTime = remindAt.getTime();
+	const now = new Date().getTime();
+	const diff = remindTime - now;
+
+	if (diff < 0) {
+		res.send('受理できず... (過去日付)');
+		return;
+	}
+
+	setTimeout((r) => {
+		r.send('遠征から帰ってくる艦隊があるよ。');
+	}, diff, res);
+
+	res.send('帰ってきたら通知します。');
+}
