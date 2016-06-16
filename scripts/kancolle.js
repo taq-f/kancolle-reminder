6009f446d7359591fc98294ec5e0700a9f7c67f5
taq-f <mysecondproject@gmail.com>
2016-06-17 07:30:33 +0900

    発言にメンションをつけるようにした

--- scripts/kancolle.js
+++ scripts/kancolle.js
@@ -11,10 +11,11 @@ const hubotSlack = require('hubot-slack');
 
 module.exports = (robot) => {
 
-	robot.listeners.push(new hubotSlack.SlackBotListener(robot, /^遠征に出発しました。帰投予定時刻 /i, (res) => {
+	robot.listeners.push(new hubotSlack.SlackBotListener(robot, /遠征に出発しました。帰投予定時刻 /i, (res) => {
 		const message = res.message.text;
-		let remindAt = message.substring('遠征に出発しました。帰投予定時刻 '.length);
-		remindAt = remindAt.split(' ').map(val => val.trim()).filter(val => val !== '');
+		const replyTo = message.substring(0, res.match.index);
+		const timeString = message.substring(res.match[0].length + res.match.index);
+		let remindAt = timeString.split(' ').map(val => val.trim()).filter(val => val !== '');
 
 		const date = remindAt[0].split('-');
 		const time = remindAt[1].split(':');
@@ -29,10 +30,23 @@ module.exports = (robot) => {
 			remindAt[5]
 		);
 
-		remind(res, remindAt);
+		const remindTime = remindAt.getTime();
+		const now = new Date().getTime();
+		const diff = remindTime - now;
+
+		if (diff < 0) {
+			res.send(replyTo + '受理できず... (過去日付)');
+			return;
+		}
+
+		setTimeout((r, to, t) => {
+			r.send(to + '遠征から艦隊が帰ってきたよ。' + t);
+		}, diff, res, replyTo, timeString);
+
+		res.send(replyTo + '帰ってきたら通知します。');
 	}));
 
-	robot.hear(/(^remind mission end at )/i, (res) => {
+	robot.hear(/^remind mission end /i, (res) => {
 
 		const message = res.message.text;
 		let remindAt = message.substring('remind mission end at '.length);
@@ -49,24 +63,19 @@ module.exports = (robot) => {
 			remindAt[4]
 		);
 
-		remind(res, remindAt);
+		const remindTime = remindAt.getTime();
+		const now = new Date().getTime();
+		const diff = remindTime - now;
+
+		if (diff < 0) {
+			res.reply('受理できず... (過去日付)');
+			return;
+		}
+
+		setTimeout((r) => {
+			r.reply('遠征から帰ってくる艦隊があるよ。');
+		}, diff, res);
+
+		res.reply('帰ってきたら通知します。');
 	});
 };
-
-function remind(res, remindAt) {
-
-	const remindTime = remindAt.getTime();
-	const now = new Date().getTime();
-	const diff = remindTime - now;
-
-	if (diff < 0) {
-		res.send('受理できず... (過去日付)');
-		return;
-	}
-
-	setTimeout((r) => {
-		r.send('遠征から帰ってくる艦隊があるよ。');
-	}, diff, res);
-
-	res.send('帰ってきたら通知します。');
-}
